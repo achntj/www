d07fae3fe4b8ca987ac23a59bb8ba679de9bf999
achntj <achntj@gmail.com>
2023-06-20 22:30:34 +0530

    fix: now-playing content layout shift

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="overflow-scroll flex text-base items-center">
+    <div className="overflow-scroll pt-5">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
