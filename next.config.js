fbb1b026a54bf972f55016324907b2d3e9be3107
achntj <achntj@gmail.com>
2023-07-09 11:00:48 +0530

    chore: add redirects from /posts/*

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/posts/:path*",
+        destination: "/writing/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
