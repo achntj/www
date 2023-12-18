d2202e5827eb32a657a29a593546943a7d4c0191
achntj <achntj@gmail.com>
2023-12-18 16:14:07 -0700

    content: update coursework

--- pages/coursework.tsx
+++ pages/coursework.tsx
@@ -8,6 +8,25 @@ export default function Uses() {
         <p>
           A list of courses I’m currently taking (and have taken in the past).
         </p>
+        <h3 className="my-5 text-2xl font-bold">Spring 24</h3>
+        <ul className="pl-5 list-disc space-y-2">
+          <li>CSE 310: Data Structures and Algorithms</li>
+          <li>CSE 301: Computing Ethics</li>
+          <li>CSE 230: Computer Org/Assemb Lang Prog</li>
+          <li>IEE 380: Prob & Stats Engr Prob Solving</li>
+          <li>BIO 130: Intro to Environmental Science</li>
+          <li>EXW 101: Found of Health & Fitness Sci</li>
+        </ul>
+        <h3 className="my-5 text-2xl font-bold">Fall 23</h3>
+        <ul className="pl-5 list-disc space-y-2">
+          <li>CSE 240: Intro to Programming Languages</li>
+          <li>CSE 120: Digital Design Fundamentals</li>
+          <li>MAT 267: Calculus for Engineers III</li>
+          <li>MAT 243: Discrete Math Structures</li>
+          <li>BIO 100: The Living World</li>
+          <li>TCL 471: Latinos In Hollywood</li>
+          <li>PUP 200: Cities in Cinema</li>
+        </ul>
         <h3 className="my-5 text-2xl font-bold">Spring 23</h3>
         <ul className="pl-5 list-disc space-y-2">
           <li>CSE 205: Object-Oriented Programming and Data Structures</li>
