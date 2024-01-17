38c0b1087e52b21a8be5147a92c5e8ff3491baef
achntj <achntj@gmail.com>
2024-01-16 19:13:11 -0700

    design: /writing redesign

--- pages/writing/index.tsx
+++ pages/writing/index.tsx
@@ -6,25 +6,59 @@ import Container from "@/components/Container";
 import { postFilePaths, POSTS_PATH } from "@/utils/mdxUtils";
 
 export default function Index({ posts }) {
+  // Group posts by year
+  const postsByYear = {};
+
+  posts.forEach((post) => {
+    const year = new Date(post.frontmatter.date).getFullYear();
+    if (!postsByYear[year]) {
+      postsByYear[year] = [];
+    }
+    postsByYear[year].push(post);
+  });
+
+  // Sort years in reverse chronological order
+  const sortedYears = Object.keys(postsByYear).sort(
+    (a, b) => Number(b) - Number(a)
+  );
+
   return (
     <Container>
       <h1 className="mb-2">Writing</h1>
       <p>Essays on topics I care about.</p>
-      <ul className="my-5 last:border-b">
-        {posts.map((post) => (
-          <Link
-            key={post.filePath}
-            href={`/writing/${post.filePath.replace(/\.mdx?$/, "")}`}
-          >
-            <li className="space-x-2 py-2 border-t flex justify-between">
-              <span>{post.frontmatter.title}</span>
-              <span className="hidden sm:inline-block">
-                {post.frontmatter.date}
-              </span>
-            </li>
-          </Link>
-        ))}
-      </ul>
+      {sortedYears.map((year) => (
+        <div key={year} className="my-4">
+          <div className="flex items-center space-x-4">
+            <span className="border-t flex-1"></span>
+            <h3 className="">{year}</h3>
+            <span className="border-t flex-1"></span>
+          </div>
+          <ul className="my-5">
+            {postsByYear[year]
+              .sort(
+                (a, b) =>
+                  Number(new Date(b.frontmatter.date)) -
+                  Number(new Date(a.frontmatter.date))
+              )
+              .map((post) => (
+                <Link
+                  key={post.filePath}
+                  href={`/writing/${post.filePath.replace(/\.mdx?$/, "")}`}
+                >
+                  <li className="py-2">
+                    <h4 className="font-normal">{post.frontmatter.title}</h4>
+                    <p className="text-sm text-neutral-800 dark:text-neutral-400">
+                      {post.frontmatter.description}
+                    </p>
+                    <p className="text-xs text-neutral-600 dark:text-neutral-500">
+                      {post.frontmatter.date}
+                    </p>
+                  </li>
+                </Link>
+              ))}
+          </ul>
+        </div>
+      ))}
     </Container>
   );
 }
