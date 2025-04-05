fc4a2c2b12788cdfb59c04994274619286702f5c
achntj <achntj@gmail.com>
2025-04-05 02:38:45 -0700

    fix: minimal navlinks

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function NavLink({ children, href, name }) {
+export default function NavLink({href, name }) {
   const router = useRouter();
   const isActive = router.asPath === href;
   return (
@@ -12,7 +12,6 @@ export default function NavLink({ children, href, name }) {
           isActive ? "underline underline-offset-4" : ""
         } navLink flex items-center space-x-4 cursor-pointer hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-lg p-2`}
       >
-        {children}
         <p className="block">{name}</p>
       </div>
     </Link>
