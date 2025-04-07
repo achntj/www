1353e32fd4f73ced04328f255690780698cc542f
achntj <achntj@gmail.com>
2025-04-07 15:45:17 -0700

    feat: smaller text [base]

--- components/Base.tsx
+++ components/Base.tsx
@@ -9,11 +9,11 @@ export default function Base({ children, ...pageProps }) {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <div className={`${font.className} min-h-screen  text-gray-700 flex-grow dark:text-[#a0a0a0] flex items-center justify-center p-4`}>
+      <div className={`${font.className} min-h-screen text-gray-700 flex-grow dark:text-[#a0a0a0] flex items-center justify-center p-4`}>
 
         <div className="w-full max-w-7xl h-[80vh] bg-[#FCF6EA] dark:bg-neutral-900 md:border-2 border-[#808F66] rounded-2xl overflow-hidden shadow-lg flex">
           <Nav />
-          <main className="flex-1 p-10 text-lg md:text-xl overflow-auto md:border-l-2 border-[#808F66]">{children}</main>
+          <main className="flex-1 p-10 text-lg md:text-lg overflow-auto md:border-l-2 border-[#808F66]">{children}</main>
         </div>
       </div>
       </>
