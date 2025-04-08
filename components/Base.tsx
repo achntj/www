c08cd4a8c82e9d655a02fc80205e4bbdda887b42
achntj <achntj@gmail.com>
2025-04-07 21:27:36 -0700

    design: fixed decor padding + order reversal

--- components/Base.tsx
+++ components/Base.tsx
@@ -25,12 +25,12 @@ export default function Base({ children, ...pageProps }) {
         className={`${font.className} min-h-screen text-gray-700 flex-grow dark:text-[#a0a0a0] md:flex items-center justify-center md:p-4`}
       >
         <div className="w-full max-w-7xl md:h-[80vh] bg-[#FCF6EA] dark:bg-neutral-900 md:border-2 border-[#808F66] md:rounded-2xl overflow-hidden shadow-lg flex relative">
-          <div className="hidden xl:block absolute top-0 left-56 z-10 pointer-events-none">
-            <Image src="/flower.png" alt="Decorative flower" width={100} height={100} className="opacity-80" />
+          <div className="hidden p-2 xl:block absolute top-0 left-56 z-10 pointer-events-none">
+            <Image src="/tree.png" alt="Decorative tree" width={100} height={100} className="opacity-80" />
           </div>
 
-          <div className="hidden xl:block absolute bottom-0 right-0 z-10 pointer-events-none">
-            <Image src="/tree.png" alt="Decorative tree" width={100} height={100} className="opacity-80" />
+          <div className="hidden p-2 xl:block absolute bottom-0 right-0 z-10 pointer-events-none">
+            <Image src="/flower.png" alt="Decorative flower" width={100} height={100} className="opacity-80" />
           </div>
 
           <Nav open={open} setOpen={setOpen} />
