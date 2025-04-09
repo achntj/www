7b885145bc4b34976cc4d5299a28e48f7956ada2
achntj <achntj@gmail.com>
2025-04-08 20:52:03 -0700

    fix: container height

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,8 +4,8 @@ export default function Container({ children, ...pageProps }) {
   return (
     <>
       <HeadContainer title={pageProps.title} description={pageProps.description} image={pageProps.image} />
-      <div className="relative h-full w-full">
-        <div className="h-full md:h-fit max-w-[700px] mx-auto py-4">{children}</div>
+      <div className="relative h-full md:h-fit w-full">
+        <div className="max-w-[700px] mx-auto py-4">{children}</div>
       </div>
     </>
   )
